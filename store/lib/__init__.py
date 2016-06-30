ada37080c8ad68e5485762624e63adff362cbfd0
Foster McLane <fkmclane@gmail.com>
2016-06-30 11:41:42 -0400

    flake8 everything

--- store/lib/__init__.py
+++ store/lib/__init__.py
@@ -1,13 +1,21 @@
+import importlib
+import os
 import sys
+
 
 sys.path.append(os.path.dirname(__file__) + '/cron.py')
 sys.path.append(os.path.dirname(__file__) + '/db.py')
 sys.path.append(os.path.dirname(__file__) + '/web.py')
 
-import cron, db, web
+
+importlib.import_module('cron')
+importlib.import_module('db')
+importlib.import_module('web')
+
 
 sys.path.remove(os.path.dirname(__file__) + '/cron.py')
 sys.path.remove(os.path.dirname(__file__) + '/db.py')
 sys.path.remove(os.path.dirname(__file__) + '/web.py')
+
 
 __all__ = ['cron', 'db', 'web']
