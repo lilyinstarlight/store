fd4d5b9940a7f34badd977f8f85903b36c36cd90
Foster McLane <fkmclane@gmail.com>
2016-07-22 14:28:36 -0400

    fix default config

--- store/config.py
+++ store/config.py
@@ -1,12 +1,12 @@
 # log locations
-log = '/home/foster/tmp/var/log/store/store.log'
-httplog = '/home/foster/tmp/var/log/store/http.log'
+log = '/var/log/store/store.log'
+httplog = '/var/log/store/http.log'
 
 # minute of hour to prune files
 minute = 9
 
 # directory to store information
-dir = '/home/foster/tmp/var/lib/store/'
+dir = '/var/lib/store/'
 
 # number of random characters to use
 random = 6
