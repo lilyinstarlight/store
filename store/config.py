9eb8df7930c281c1504c5d41b288e5636637f4f9
Foster McLane <fkmclane@gmail.com>
2016-07-22 23:31:58 -0400

    reorder config file

--- store/config.py
+++ store/config.py
@@ -1,3 +1,9 @@
+# address to listen on
+addr = ('', 8080)
+
+# directory to store information
+dir = '/var/lib/store/'
+
 # log locations
 log = '/var/log/store/store.log'
 httplog = '/var/log/store/http.log'
@@ -5,11 +11,5 @@ httplog = '/var/log/store/http.log'
 # minute of hour to prune files
 minute = 9
 
-# directory to store information
-dir = '/var/lib/store/'
-
 # number of random characters to use
 random = 6
-
-# address to listen on
-addr = ('', 8080)
